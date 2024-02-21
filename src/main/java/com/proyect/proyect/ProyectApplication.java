a6a4935c68a82f96c64512f3bd8d4fa405da0eb0
juanpabloay <juanpabloantonyagual@gmail.com>
2024-02-21 01:17:02 -0500

    master: modificacion de configuracion para el front

--- src/main/java/com/proyect/proyect/ProyectApplication.java
+++ src/main/java/com/proyect/proyect/ProyectApplication.java
@@ -1,12 +1,12 @@
 package com.proyect.proyect;
 
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class ProyectApplication {
+public class ProyectApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectApplication.class, args);
@@ -16,5 +16,12 @@ public class ProyectApplication {
 	// public Message newmessage(){
 	// return new Message(LocalDateTime.now(), "Hola mundooooooooo desde sprint");
 	// }
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("http://localhost:3003") // Permitir solicitudes desde este origen
+				.allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
+				.allowedHeaders("*"); // Encabezados permitidos
+	}
 
 }
